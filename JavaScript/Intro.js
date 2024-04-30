4dd85f1362e340ceb0fdbca0e81fa1362afa6031
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-05-01 00:10:10 +0530

    Intro Done

--- JavaScript/Intro.js
+++ JavaScript/Intro.js
@@ -1,5 +1,32 @@
 var a =Symbol('hi')
 var b =Symbol('hi')
-if (a==b) {
-    console.log("true")
+console.log(a==b)
+
+var car={
+    owner:"Himanshu",
+    model:'BWM',
+    year:2024,
+    drive:function(){
+
+    }
 }
+var student={
+    name:'Himanshu',
+    rollno:123,
+    marks:90,
+    addres:{
+        City:'Delhi',
+        State:'Delhi'
+    }
+
+}
+// student.addres
+// student['name']
+// student[marks]
+
+console.log(student.name)
+
+var marks =[10,20,3,30,40,50]
+console.log(marks[1])
+marks[1]=21
+console.log(marks)
