a8a80242f387b0ab1b833938029852cc4e1fc821
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-05-09 02:24:58 +0530

    Conditions Done

---
+++ JavaScript/ConditionLoops.js
@@ -0,0 +1,15 @@
+console.log("Hello World")
+
+let age=12
+if(age>=18){
+    console.log("You are eligible to vote")
+}
+else{
+    console.log("You are not eligible to vote")
+}
+
+// Ternary  operator
+let a=10
+a===10? console.log("a is equal"):console.log('not equal')
+
+// Loops
