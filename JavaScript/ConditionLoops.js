a509b31e8d0e6184ce2016c2c859acd67cd9547a
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-05-09 02:32:12 +0530

    loops Done

--- JavaScript/ConditionLoops.js
+++ JavaScript/ConditionLoops.js
@@ -13,3 +13,25 @@ let a=10
 a===10? console.log("a is equal"):console.log('not equal')
 
 // Loops
+
+// for loops
+// a=10
+// for(i=1;i<=a;i++){
+//     console.log(a*i)
+// }
+
+// while loops
+// let x=10
+// let b=1
+// while(b<=x){
+//     console.log(x*b)
+//     b++
+// }
+
+// do while
+let x=10
+b=1
+do {
+    console.log(a)
+    b++
+} while (b<x);
