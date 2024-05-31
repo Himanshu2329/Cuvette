3d46bbf013ae07e47d8dac14dde50a71da980664
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-05-31 17:50:13 +0530

    Event Listener, styling, innertext Done

--- DOM/script.js
+++ DOM/script.js
@@ -13,3 +13,33 @@ console.log(p)
 var span=document.querySelectorAll('span')
 console.log(span[0])
 console.log(span[1])
+
+var para = document.querySelector('p#para')
+// console.log(para.innerHTML)
+// console.log(para.innerText)
+para.innerHTML='bye'
+// para.innerHTML='<p style="color:red;">hi</p>'
+
+// para.style.background='grey'
+
+
+// Mouse Events
+var btn = document.getElementById('btn')
+btn.addEventListener('click',function(){
+  console.log('hii')
+  para.style.color='red'
+  para.style.background='yellow'
+  para.innerText='hello buddy'
+  
+})
+
+para.addEventListener('mouseover', function(){
+      console.log("moving mouse over p tag")                  
+})
+
+btn.addEventListener('dblclick', function(){
+  console.log(para.innerText='im clicked twice')
+})
+
+// Keyboard events
+
