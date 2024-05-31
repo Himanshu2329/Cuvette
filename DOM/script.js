44b98fda8d14b2650dd0bad5000418abf101eb1e
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-05-31 15:52:56 +0530

    DOM start

---
+++ DOM/script.js
@@ -0,0 +1,15 @@
+// var div1=document.getElementById('div1')
+// var paras = document.getElementsByClassName('para')
+// console.log(paras[0]);
+
+var pele=document.getElementsByTagName('p')
+console.log(pele);
+
+// Use query selector
+
+var p = document.querySelector('p.para')
+console.log(p)
+
+var span=document.querySelectorAll('span')
+console.log(span[0])
+console.log(span[1])
