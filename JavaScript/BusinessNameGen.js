12249d45fe598ebef0b4e3d11a63771c39f11288
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-06-09 22:21:01 +0530

    BusinessNameGen

---
+++ JavaScript/BusinessNameGen.js
@@ -0,0 +1,73 @@
+let obj1 = {
+    1: "Crazy",
+    2: "Amazing",
+    3: "Fire",
+    4:"Bored"
+    
+}
+let obj2 = {
+    1: "Food",
+    2: "Garments",
+    3: "Wine",
+    4:"exam"
+}
+let obj3 = {
+    1: "don",
+    2: "world",
+    3: "private",
+    4:"final"
+}
+let obj4 = {
+    1: "LTD",
+    2: ".gov",
+    3: ".In",
+    4:"result"
+}
+// let car = Math.floor(Math.random()*4)+1
+// console.log(car);
+var ran1 = Math.floor(Math.random()*4)+1
+var ran2 = Math.floor(Math.random()*4)+1
+var ran3 = Math.floor(Math.random()*4)+1
+var ran4 = Math.floor(Math.random()*4)+1
+
+console.log(obj1[ran1],obj2[ran2],obj3[ran3],obj4[ran4]);
+
+// let a,b,c
+
+// let ran=Math.random()
+
+// if(ran<.33){
+//     a="crazy";
+// }
+// if(ran<=.33&&ran<.66){
+//     a="Amazing";
+// }
+// else{
+//     a="Fire"
+// }
+
+// ran=Math.random()
+
+// if(ran<.33){
+//     b="Food";
+// }
+// if(ran<=.33&&ran<.66){
+//     b="Garments";
+// }
+// else{
+//     b="Wine"
+// }
+
+// ran=Math.random()
+
+// if(ran<.33){
+//     c="LTD";
+// }
+// if(ran<=.33&&ran<.66){
+//     c=".gov";
+// }
+// else{
+//     c=".IN"
+// }
+
+// console.log(`${a} ${b} ${c}`);
