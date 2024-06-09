1e5e6c9fc5ba075bffd3335928d6fe1c7b56d020
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-06-09 22:20:35 +0530

    String

---
+++ JavaScript/Strings.js
@@ -0,0 +1,8 @@
+let realname= "Himanshu"
+let friend = "Rohan"
+console.log(("His name is " + realname + " and his friend name is "+friend));
+console.log(`His name is ${realname} and his friend name is ${friend }`);
+console.log(realname.toUpperCase());
+console.log(realname.toLowerCase());
+console.log(friend.slice(1,4)) //last digit is excluded
+console.log(realname.slice(0,5));
